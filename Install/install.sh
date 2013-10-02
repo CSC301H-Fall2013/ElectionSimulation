80bc666377c77d157da19baa10f76d8eec219b12
g2minhle <9ihchooc>
2013-10-02 00:24:21 -0400

    Fix one-click installer.

--- Install/install.sh
+++ Install/install.sh
@@ -1,12 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 
-tar xvjf Python-2.7.5.tar.bz2
-cd Python-2.7.5
-./configure
-make
+if test -z `which python`
+then
+    tar xvjf Python-2.7.5.tar.bz2
+    cd Python-2.7.5
+    ./configure
+    make
+    cd ..
+fi
 
-
-cd ..
 tar xzvf Django-1.5.4.tar.gz
 cd Django-1.5.4
 sudo python setup.py install
