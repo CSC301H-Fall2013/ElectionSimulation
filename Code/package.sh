6881e049b4e819a8a85118d05ab0b0a2f426e771
g2minhle <9ihchooc>
2013-10-02 20:55:30 -0400

    Add package.sh

---
+++ Code/package.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Clean the install folder
+# Push all the code from code folder to Install folder
+
+chmod 700 *
+InstallerFolder=ElectionSimulationInstaller
+rm -rf ../${InstallerFolder}/ElectionSimulation/
+mkdir ../${InstallerFolder}/ElectionSimulation/
+cp -r ./ElectionSimulation/ ../${InstallerFolder}/ElectionSimulation/
