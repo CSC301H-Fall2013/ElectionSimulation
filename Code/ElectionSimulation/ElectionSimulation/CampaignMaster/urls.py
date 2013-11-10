7e5f42bfb2182646c28b3d11e2eba6d64725271f
g2minhle <lehoangminh@live.com>
2013-11-10 15:50:16 -0500

    Add a place holder for new campaign form

--- Code/ElectionSimulation/ElectionSimulation/CampaignMaster/urls.py
+++ Code/ElectionSimulation/ElectionSimulation/CampaignMaster/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import patterns, url
 from ElectionSimulation.CampaignMaster import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.listCampaign, name='listCampaign') # Show the list of current campaigns
+    url(r'^$', views.list_campaign, name='list_campaign'), # Show the list of current campaigns
+    url(r'^add/$', views.add_new_campaign, name='add_new_campaign'), # Show the list of current campaigns
 )
--- Code/ElectionSimulation/ElectionSimulation/CampaignMaster/views.py
+++ Code/ElectionSimulation/ElectionSimulation/CampaignMaster/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 from ElectionSimulation.Models.Campaign import Campaign
 
-def listCampaign(request):
+
+def list_campaign(request):
     """ Get the list of all campaigns and render the list
     """
 
-    #c = Campaign(name="What's new?", create_date=timezone.now())
     context = {'campaigns': Campaign.objects.all()}
     return render(request, 'CampaignMaster/list.html', context)
+
+
+def add_new_campaign(request):
+    """ Add a new campaign
+    """
+
+    return render(request, 'CampaignMaster/addNewCampaign.html')
